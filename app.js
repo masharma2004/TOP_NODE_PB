eac4e4ab148a1efa01bd088c3e4f20dd85d82dde
Mayank Sharma <36720230+masharma2004@users.noreply.github.com>
2024-05-11 12:10:23 +0530

    final commit

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-app.use(express.static(__dirname));
-app.use('*',(req,res)=>{
-    res.redirect("/404.html");
-});
 
+
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/about',(req,res)=>{
+    res.sendFile(__dirname + '/about.html');
+});
+app.get('/contact-me',(req,res)=>{
+    res.sendFile(__dirname + '/contact-me.html');
+});
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + '/404.html');
+  });
 app.listen(PORT, ()=>{
-    console.log("Server is running and listening on ${PORT}");
+    console.log(`Server is running and listening on ${PORT}`);
 });
